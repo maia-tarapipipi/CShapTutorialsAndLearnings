6c9ba03f8bbf8b2c573c0e325914fbed488279e3
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-10 13:49:17 +1300

    reconfig to include system and class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program {
+  static void Main(string[] args) 
+  {
+  System.Console.WriteLine("Hello World");  
+
+  }
+  }
+
