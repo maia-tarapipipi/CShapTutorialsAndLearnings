7f4dd7289b5810e3696e16e24e3a5563fa16c5fc
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-20 12:10:29 +1300

    overloaded methods ex.2

--- Program.cs
+++ Program.cs
@@ -6,25 +6,12 @@ class Program
 {
   static void Main(string[] args) 
   {
-    // Random dice = new Random();
-    // int roll = dice.Next(1, 7);
-    // Console.WriteLine(roll);
+    int firstValue = 500;
+    int secondValue = 600;
+    int largerValue = Math.Max(firstValue, secondValue);
 
-    // int number = 7;
-    // string text = "seven";
+    Console.WriteLine(largerValue);
 
-    // Console.WriteLine(number);
-    // Console.WriteLine();
-    // Console.WriteLine(text);
-
-    Random dice = new Random();
-    int roll1 = dice.Next();
-    int roll2 = dice.Next(101);
-    int roll3 = dice.Next(50, 101);
-
-    Console.WriteLine($"First roll: {roll1}");
-    Console.WriteLine($"Second roll: {roll2}");
-    Console.WriteLine($"Third roll: {roll3}");
   } 
   
 }
