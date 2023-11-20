fc8bbf3519be9d8504804574772c23c84c683e18
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-20 13:55:55 +1300

    array basics challenge

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ class Program
 {
   static void Main() 
   {
-    int[] inventory = { 200, 450, 700, 175, 250 };
-    int sum = 0;
-    int bin = 0;
-    foreach (int items in inventory)
+    string[] fraudulentOrderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+    foreach (string item in fraudulentOrderID)
     {
-        sum += items;
-        bin++;
-        Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-    }
-    Console.WriteLine($"We have {sum} items in inventory.");
+      if (item.StartsWith("B"))
+      {
+        Console.WriteLine(item);
+      }
+    };
+
   } 
   
 }
