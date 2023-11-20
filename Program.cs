5aeace1503390d5161939c85180f6e0c3b08a0f9
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-20 13:15:19 +1300

    if else statements refactored

--- Program.cs
+++ Program.cs
@@ -6,35 +6,25 @@ class Program
 {
   static void Main(string[] args) 
   {
-     Random dice = new Random();
+     Random random = new Random();
+    int daysUntilExpiration = random.Next(12);
+    int discountPercentage = 0;
 
-    int roll1 = dice.Next(1, 7);
-    int roll2 = dice.Next(1, 7);
-    int roll3 = dice.Next(1, 7);
-
-    int total = roll1 + roll2 + roll3;
-
-    Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-    if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+    if (daysUntilExpiration < 10) 
     {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
+      System.Console.WriteLine("Your subscription will expire soon. Renew now!");
     }
-
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
+    else if (daysUntilExpiration < 5) 
+    { 
+      System.Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
     }
-
-    if (total >= 15)
-    {
-        Console.WriteLine("You win!");
+    else if (daysUntilExpiration == 1)
+    { 
+      System.Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
     }
-    else 
+    else if (daysUntilExpiration < 1)
     {
-        Console.WriteLine("Sorry, you lose.");
+      System.Console.WriteLine("Your subscription has expired.");
     }
 
   } 
