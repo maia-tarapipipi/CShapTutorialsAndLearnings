1deeda8bcb28a7948d81e7457378dbad70d22aa9
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-20 13:22:20 +1300

    if else statements refactored

--- Program.cs
+++ Program.cs
@@ -9,12 +9,12 @@ class Program
      Random random = new Random();
     int daysUntilExpiration = random.Next(12);
     int discountPercentage = 0;
-
-    if (daysUntilExpiration < 10) 
+    
+    if (daysUntilExpiration >= 5 && daysUntilExpiration <= 10) 
     {
       System.Console.WriteLine("Your subscription will expire soon. Renew now!");
     }
-    else if (daysUntilExpiration < 5) 
+    else if (daysUntilExpiration >= 2 && daysUntilExpiration <= 5) 
     { 
       System.Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
     }
@@ -22,7 +22,7 @@ class Program
     { 
       System.Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
     }
-    else if (daysUntilExpiration < 1)
+    else if (daysUntilExpiration == 0)
     {
       System.Console.WriteLine("Your subscription has expired.");
     }
