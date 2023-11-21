7e40c53558ef9178432add4a92bf5dd591920f78
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-21 14:54:54 +1300

    coin challenge upated logic

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ class Program
   static void Main(string[] args)
     {
         Random coin = new Random();
-        int flip = coin.Next(0, 10);
+        // int flip = coin.Next(0, 800);
 
-        System.Console.WriteLine($"{(flip % 2 == 0 ? $"heads {flip}" : $"tails{flip}")}");
+        System.Console.WriteLine((coin.Next(0, 100) % 2 == 0 ? "heads" : "tails"));
     }
 
 }
