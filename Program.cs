aecbbbe853a22938f3aa28d2e684cb3993ab2a0e
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 10:42:59 +1300

    switch case for reference

--- Program.cs
+++ Program.cs
@@ -3,14 +3,60 @@ class Program
 {
   static void Main(string[] args)
     {
-      int value = 5;
+   // SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-      if (value > 0)
-        {
-          int value2 = 6;
-        }
-        value = value + value2;
-        System.Console.WriteLine(value);
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+  case "01": 
+    type = "Sweat shirt";
+    break;
+  case "02":
+    type = "T-Shirt";
+    break;
+  case "03":
+    type = "Sweat pants";
+    break;
+  default: 
+    type = "Other";
+    break;
+}
+switch (product[1])
+{
+  case "BL": 
+    color = "Black";
+    break;
+  case "MN":
+    color = "Maroon";
+    break;
+  default: 
+     color = "White";
+    break;
+}
+switch (product[2])
+{
+  case "S": 
+     size = "Small";
+    break;
+  case "M":
+    size = "Medium";
+    break;
+  case "L":
+    size = "Large"; 
+    break;
+  default: 
+     size = "One Size Fits All";
+    break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
     }
 
 }
