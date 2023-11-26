57b57b9f01e0a1df38546de5b2d906de4dfa3489
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 10:27:38 +1300

    if block knowledge check challenge

--- Program.cs
+++ Program.cs
@@ -3,14 +3,13 @@ class Program
 {
   static void Main(string[] args)
     {
-      int value = 1;
+      int value = 5;
 
       if (value > 0)
         {
-          int value2 = 8;
-          value = value + value2;
-
+          int value2 = 6;
         }
+        value = value + value2;
         System.Console.WriteLine(value);
     }
 
