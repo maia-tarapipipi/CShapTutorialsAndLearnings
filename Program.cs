f6fd3af914bfb7836154e932467949d4d729c17e
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 11:36:12 +1300

    do while general notes

--- Program.cs
+++ Program.cs
@@ -3,18 +3,38 @@ class Program
 {
   static void Main(string[] args)
     {
-   
-   for (int i = 1; i <= 100; i++)
-   {
-    if (i % 15 == 0)
-    System.Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 5 == 0) 
-    System.Console.WriteLine($"{i} - Buzz");
-    else if (i % 3 == 0) 
-    System.Console.WriteLine($"{i} - Fizz");
-    else 
-    System.Console.WriteLine(i);
-   };
+ // do while 
+ 
+Random random = new Random();
+int current = random.Next(1, 11);
+
+/*
+do
+{
+ // will execute at least once -> large number while the code within the "while" code block is true
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+*/
+
+// positioning the boolean before the code block makes the block act as a gate for the expression to only allow the flow of execution to enter if the bool expression evaluates to true 
+// while (current >= 3)
+// {
+//     Console.WriteLine(current);
+//     current = random.Next(1, 11);
+// }
+// Console.WriteLine($"Last number: {current}");
+
+// Continue is the important part of the code block ensuring that the statement transfers execution to the end of the current iteration.
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
     }
 
 }
