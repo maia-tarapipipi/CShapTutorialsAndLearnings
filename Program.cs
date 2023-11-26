55aeb5e19229e6c84dc12686f962cc2432552bbb
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 11:22:14 +1300

    Fizzbuzz challenge

--- Program.cs
+++ Program.cs
@@ -3,60 +3,18 @@ class Program
 {
   static void Main(string[] args)
     {
-   // SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-switch (product[0])
-{
-  case "01": 
-    type = "Sweat shirt";
-    break;
-  case "02":
-    type = "T-Shirt";
-    break;
-  case "03":
-    type = "Sweat pants";
-    break;
-  default: 
-    type = "Other";
-    break;
-}
-switch (product[1])
-{
-  case "BL": 
-    color = "Black";
-    break;
-  case "MN":
-    color = "Maroon";
-    break;
-  default: 
-     color = "White";
-    break;
-}
-switch (product[2])
-{
-  case "S": 
-     size = "Small";
-    break;
-  case "M":
-    size = "Medium";
-    break;
-  case "L":
-    size = "Large"; 
-    break;
-  default: 
-     size = "One Size Fits All";
-    break;
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
+   
+   for (int i = 1; i < 30; i++)
+   {
+    if (i % 15 == 0)
+    System.Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 5 == 0) 
+    System.Console.WriteLine($"{i} - Buzz");
+    else if (i % 3 == 0) 
+    System.Console.WriteLine($"{i} - Fizz");
+    else 
+    System.Console.WriteLine(i);
+   };
     }
 
 }
