ba835636f32e952ea32b4c1a83349bd8cc554b2c
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 15:22:48 +1300

    do while project one - validation integers - adjusts int validation

--- Program.cs
+++ Program.cs
@@ -15,18 +15,23 @@ class Program
         readResult = System.Console.ReadLine();
         int integer = 0;
         bool validNumber = false;
-        validNumber = int.TryParse(readResult, out integer);
-
+        if (validNumber = int.TryParse(readResult, out integer)) 
+        {
         if (integer > 5 && integer < 10) 
         { 
           validEntry = true;
         } else 
         {
-          System.Console.WriteLine("The integer you have submitted is incorrect please submit a valid integer");    
+          System.Console.WriteLine("The integer you have submitted is incorrect, please submit a valid integer");    
         }
       
+        } else
+        {
+          System.Console.WriteLine("Please submit a valid integer between 5 and 10");
+        };
+     
       } while (validEntry == false);
 
-      System.Console.WriteLine("The integer you have submitted has been accepted");
+      System.Console.WriteLine($"Your input of ({readResult}) has been accepted");
     }     
 }
