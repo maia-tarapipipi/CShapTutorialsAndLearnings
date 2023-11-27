788e29e1f3294c97ba2adad221d29464512ab673
maia-tarapipipi <tarapipipim@gmail.com>
2023-11-27 13:49:26 +1300

    monster hero battle challenge my solution

--- Program.cs
+++ Program.cs
@@ -3,37 +3,39 @@ class Program
 {
   static void Main(string[] args)
     {
- // do while 
- 
-Random random = new Random();
-int current = random.Next(1, 11);
+      
+      int heroHealth = 10;
+      int monsterHealth = 10;
+      Random attack = new Random();
+       
+     do
+     {
+      if (monsterHealth > 0)
+      {
+        int damage = attack.Next(1, 10);
+        monsterHealth = monsterHealth - (int)damage;
+        System.Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+        if (monsterHealth <= 0) 
+        {
+        System.Console.WriteLine("Hero Wins!");
+        }
+      };
+       if (monsterHealth > 0)
+      {
+      int damage = attack.Next(1, 10);
+      heroHealth = heroHealth - damage;
+      System.Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");  
+      if (heroHealth <= 0 ){
+      System.Console.WriteLine("Monster Wins!");
+      }
+      };
+      
+     } while (monsterHealth > 0 && heroHealth > 0);
 
-/*
-do
-{
- // will execute at least once -> large number while the code within the "while" code block is true
-    current = random.Next(1, 11);
-    Console.WriteLine(current);
-} while (current != 7);
-*/
 
-// positioning the boolean before the code block makes the block act as a gate for the expression to only allow the flow of execution to enter if the bool expression evaluates to true 
-// while (current >= 3)
-// {
-//     Console.WriteLine(current);
-//     current = random.Next(1, 11);
-// }
-// Console.WriteLine($"Last number: {current}");
 
-// Continue is the important part of the code block ensuring that the statement transfers execution to the end of the current iteration.
-do
-{
-    current = random.Next(1, 11);
-
-    if (current >= 8) continue;
-
-    Console.WriteLine(current);
-} while (current != 7);
+      
+      
 
     }
 
